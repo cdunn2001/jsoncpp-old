81ddef5e4351892f65bfe2a146e0be4bf5361432
Christopher Dunn <cdunn2001@gmail.com>
2011-06-21 14:29:49 -0700

    Changed copyright dates.

--- src/lib_json/json_reader.cpp
+++ src/lib_json/json_reader.cpp
@@ -1,4 +1,4 @@
-// Copyright 2011 Baptiste Lepilleur
+// Copyright 2007-2011 Baptiste Lepilleur
 // Distributed under MIT license, or public domain if desired and
 // recognized in your jurisdiction.
 // See file LICENSE for detail or copy at http://jsoncpp.sourceforge.net/LICENSE
